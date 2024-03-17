40ce074339060cd9084ec99dfb2a8c8ce374451a
andrew hulme <andrew_hulme04@hotmail.com>
2024-03-17 11:51:28 +1100

    Fixed errors

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # get "tasks", to: "tasks#list"
-  get "tasks/new", to: "task#new", as: :new_task
+  get "tasks/new", to: "tasks#new", as: :new_task
   post "tasks", to: "tasks#create"
   get "tasks/:id", to: "tasks#show", as: :task
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
